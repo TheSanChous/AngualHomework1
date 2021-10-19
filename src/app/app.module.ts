c6953c0c0fb697d41777e6a97788565caa9a576e
Alexandr Novitskiy <sashanovi77@gmail.com>
2021-10-19 20:19:48 +0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,23 @@ import {HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { UserListComponent } from './components/user-list/user-list.component';
+import { UserItemComponent } from './components/user-item/user-item.component';
+import {FormsModule} from "@angular/forms";
+import { UserAddComponent } from './components/user-add/user-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent
+    UserListComponent,
+    UserItemComponent,
+    UserAddComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
